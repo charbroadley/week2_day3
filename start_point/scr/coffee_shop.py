f7194c04acc4fb3c22758858b803d782c93bfc9b
Char Broadley <broadleychar@gmail.com>
2022-11-30 16:35:33 +0000

    finished MVP

--- start_point/scr/coffee_shop.py
+++ start_point/scr/coffee_shop.py
@@ -9,7 +9,14 @@ class CoffeeShop:
     def till_increases (self, cash):
         self.till += cash
 
-    def can_buy_drink (self, drink):
-        for name_of_drink in self.collection_of_drinks:
-            if name_of_drink == drink:
-                customer.reducing_wallet (self, )
+    def remove_stock(self, drink_selection):
+        for drink in self.collection_of_drinks:
+            if drink == drink_selection:
+                self.collection_of_drinks.remove(drink_selection)
+                break
+
+    def sell_drink (self, customer, drink_selection):
+        customer.reducing_wallet(drink_selection.price)
+        self.till_increases(drink_selection.price)
+        self.remove_stock(drink_selection)
+
--- start_point/tests/coffee_shop_tests.py
+++ start_point/tests/coffee_shop_tests.py
@@ -5,10 +5,13 @@ from scr.drink import Drink
 
 class TestCoffeeShop(unittest.TestCase):
     def setUp (self):
-        collection_of_drinks = [{"Name" : "Mocha", "Price" :3.00}, {"Name" : "Tea", "Price" : 2.00}, {"Name" : "Hot Chocolate", "Price" : 4.00}, {"Name" : "Coffee", "Price" : 2.50}]
-
-        self.drink= Drink("Mocha", 3.00)
-        self.coffee_shop = CoffeeShop("Kosta Koffee", 100.00)
+        self.drink1= Drink("Mocha", 3.00)
+        self.drink2= Drink("Tea", 2.50)
+        self.drink3= Drink("coffee", 2.00)
+        self.drink4= Drink("Hot chocolate", 4.00)
+        collection_of_drinks = [self.drink1, self.drink2, self.drink3, self.drink4]
+        self.coffee_shop = CoffeeShop("Kosta Koffee", 100.00, collection_of_drinks)
+        
 
     def test_coffee_shop_has_name (self):
         self.assertEqual ("Kosta Koffee", self.coffee_shop.name)
@@ -19,14 +22,17 @@ class TestCoffeeShop(unittest.TestCase):
     def test_till_increases (self):
         self.coffee_shop.till_increases(10.00)
         self.assertEqual (110.00, self.coffee_shop.till)
+    
+    def test_remove_drink_by_name(self):
+        self.coffee_shop.remove_stock(self.drink1)
+        self.assertEqual (3,len(self.coffee_shop.collection_of_drinks))
 
-    def 
+    def test_shop_sells_drink (self):
+        customer = Customer("Charlotte", 10.00)
+        self.coffee_shop.sell_drink(customer, self.drink1)
+        self.assertEqual(7.00, customer.wallet)
+        self.assertEqual(103.00, self.coffee_shop.till)
+        self.assertEqual(3, len(self.coffee_shop.collection_of_drinks))
 
-    def test_customer_can_buy_drink (self):
-        drink_1 = Drink ("Mocha", 3.00)
-        Customer.reducing_wallet (drink_1)
-        CoffeeShop.till_increases (drink_1)
-        self.buy_drink (customer, drink_selection)
-        self.assertEqual(7.00, Customer.wallet)
-        self.assertEqual(103.00, CoffeeShop.till)
+
         
